9b45dc0c123676681c96bcb6c4fd9844a77383c9
Biplabbrout <biplabrahul4567@gmail.com>
2025-05-13 23:39:26 +0530

    Add files via upload

---
+++ OnetoN.java
@@ -0,0 +1,21 @@
+//how to print name n times from 1 to n
+//here cant have same appraoch as printing numbers as string and int are in compatible types
+import java.util.Scanner;
+public class OnetoN {
+    
+static void print(int i,int n){
+    if(i>n) return;
+    System.out.println(i);
+    print(i+1,n);
+
+}    
+public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    System.out.print("how many times u want to print:");
+    int n= sc.nextInt();
+    sc.close();
+
+    print(1,n);
+
+}
+}
