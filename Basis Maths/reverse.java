070438b96a6c7e9a5a2baa6aa1ba726adba9c86e
Biplabbrout <biplabrahul4567@gmail.com>
2025-06-25 12:39:17 +0530

    Update reverse.java

--- Basis Maths/reverse.java
+++ Basis Maths/reverse.java
@@ -20,3 +20,26 @@ public class reverse {
     }
     
 }
+// import java.util.*;
+// class Solution {
+//     public int reverse(int x) {
+//       int rev = 0;
+        
+//         while (x != 0) {
+//             int digit = x % 10;
+//             x /= 10;
+
+//             // Check for overflow before multiplying/adding
+//             if (rev > Integer.MAX_VALUE  || (rev == Integer.MAX_VALUE && digit > 7)) {
+//                 return 0;
+//             }
+//             if (rev < Integer.MIN_VALUE  || (rev == Integer.MIN_VALUE && digit < -8)) {
+//                 return 0;
+//             }
+
+//             rev = rev * 10 + digit;
+//         }
+
+//         return rev;
+//     }
+// }
