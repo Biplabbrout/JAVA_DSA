a9487f9b594c57b77403e497ab62653ca4d4f657
Biplabbrout <biplabrahul4567@gmail.com>
2025-07-02 17:25:52 +0530

    Update largest.java

--- Arrays/Easy/largest.java
+++ Arrays/Easy/largest.java
@@ -2,26 +2,26 @@
 import java.util.*;
 
 public class largest {
-    public static void main(String[] args) {
-        // int[] arr= {1,2,3,5,66};
-        Scanner sc =new Scanner(System.in);
-        int[] arr= new int[5];
 
-        for(int i=0;i<arr.length;i++){
-            arr[i]=sc.nextInt();
-        }
-
+    public static int largestElementArray(int[] arr){
+        //{8,5,7,9,10}
         int max=arr[0];
 
-        for(int i=0;i<arr.length;i++){
+        for(int i=1;i<arr.length;i++){
             
             if(arr[i]>max){
                 max=arr[i];
             }
+
         }
+        return max;
+    }
+    public static void main(String[] args) {
+        
+        int [] arr ={8,5,7,9,10};
+        int max=largestElementArray(arr);
+
         System.out.println(max);
-
-
     }
     
 }
